9c566112bed785e5e93a837356cd09ff85bdf166
Aghogho <monorienaghogho@gmail.com>
2023-07-26 10:16:53 +0000

    3. LRU

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""
+LRU Cache
+"""
+from threading import RLock
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """
+    LRU Cache
+    """
+    def __init__(self):
+        """
+        Initialize
+        """
+        BaseCaching.__init__(self)
+        self.__keys = []
+        self.__rlock = RLock()
+
+    def put(self, key, item):
+        """
+        Add Items
+        """
+        if key and item:
+            out = self._balance(key)
+            with self.__rlock:
+                self.cache_data.update({key: item})
+            if out:
+                print("DISCARD: {}".format(out))
+
+    def get(self, key):
+        """
+        Get Items
+        """
+        with self.__rlock:
+            data = self.cache_data.get(key, None)
+            if key in self.__keys:
+                self._balance(key)
+        return data
+
+    def _balance(self, keyIn):
+        """
+        Remove Items
+        """
+        out = None
+        with self.__rlock:
+            n = len(self.__keys)
+            if keyIn not in self.__keys:
+                if len(self.cache_data) == BaseCaching.MAX_ITEMS:
+                    out = self.__keys.pop(0)
+                    self.cache_data.pop(out)
+            else:
+                self.__keys.remove(keyIn)
+            self.__keys.insert(n, keyIn)
+        return out
