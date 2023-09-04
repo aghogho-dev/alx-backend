18eefb8eec836158dfe9c8f4becf3a3347eb0d45
Aghogho <monorienaghogho@gmail.com>
2023-09-04 23:54:07 +0000

    4. force locale with URL param

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -22,6 +22,9 @@ babel = Babel(app)
 @babel.localeselector
 def get_locale() -> str:
     """Get locale"""
+    loc = request.args.get('locale', None)
+    if loc and loc in Config.LANGUAGES:
+        return loc
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
